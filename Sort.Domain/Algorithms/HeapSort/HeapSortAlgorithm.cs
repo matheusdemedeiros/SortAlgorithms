a76d2e98ca242dcffe07875236e96dd66e927f9a
Matheus de Medeiros <matheus.medeiros@ndd.tech>
2025-10-05 11:39:24 -0300

    feat: inicia a implementação do heapsort

---
+++ Sort.Domain/Algorithms/HeapSort/HeapSortAlgorithm.cs
@@ -0,0 +1,32 @@
+﻿using Sort.Domain.Algorithms.Abstractions;
+using Sort.Domain.Algorithms.Shared;
+
+namespace Sort.Domain.Algorithms.HeapSort
+{
+    /// <summary>
+    /// Implementation of the HeapSort sorting algorithm.
+    /// </summary>
+    public class HeapSortAlgorithm : ISortAlgorithm
+    {
+        /// <summary>
+        /// Sorts a list of integers using the HeapSort algorithm.
+        /// </summary>
+        /// <param name="numbers">The list of integers to be sorted.</param>
+        /// <returns>
+        /// A <see cref="SortResult"/> containing the sorted list and the operation status.
+        /// </returns>
+        public SortResult Sort(List<int> numbers)
+        {
+            var sorted = new SortResult();
+            sorted.SortedList = Process(numbers);
+            sorted.Success = sorted.SortedList != null && sorted.SortedList.Any();
+
+            return sorted;
+        }
+
+        private List<int>? Process(List<int> numbers)
+        {
+            return null;
+        }
+    }
+}
