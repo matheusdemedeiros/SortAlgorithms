827c66f97428bb4c5678b72c4fc18230531ee908
Matheus de Medeiros <matheus.medeiros@ndd.tech>
2025-10-05 11:46:15 -0300

    chore: cria projeto para instalar o pacote nuget como teste

---
+++ Sort.Package.Instalation.Test.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using Sort.Domain.Algorithms.Quicksort;
+
+namespace Sort.Package.Instalation.Test.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          var list = new List<int> { 5, 3, 8, 1, 2 };
+           
+            var sorter = new QuickSortAlgorithm();
+            var result = sorter.Sort(list);
+        }
+    }
+}
